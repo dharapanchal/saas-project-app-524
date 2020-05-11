f70ff3211a31320ad9a0cd2ab61c5ac32de9b04f
Dhara Panchal <dhara.panchal@einfochips.com>
2020-05-11 17:12:35 +0530

    setup email

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address => 'smtp.sendgrid.net',
+  :port => '587',
+  :authentication => :plain,
+  :user_name => ENV['apikey'],
+  :password => ENV['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'],
+  :domain => 'heroku.com',
+  :enable_starttls_auto => true
+}
